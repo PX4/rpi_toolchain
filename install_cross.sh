85f0edbb2dcf649816b0cb165d9116b5b8f49dc9
Kabir Mohammed <mhkabir98@gmail.com>
2016-01-17 22:14:34 +0530

    Add native and cross-compiler toolchain installers

---
+++ install_cross.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+echo "Installing PX4 Cross-Compile Toolchain for Raspberry Pi"
+echo "======================================================="
+
+TOOLCHAIN_DIR="$1"
+if [ "${TOOLCHAIN_DIR}" == "" ]; then
+  echo -e "No install path specified, installing to default location at /opt/rpi_toolchain/\n"
+  TOOLCHAIN_DIR="/opt/rpi_toolchain"
+else
+  echo "Installing cross-compiler to $TOOLCHAIN_DIR"
+fi
+
+git clone --depth 1 https://github.com/raspberrypi/tools.git rpi_toolchain
+if [ $? -eq 0 ]; then
+    echo "Toolchain downloaded successfully"
+else
+    echo "Toolchain download failure"
+    exit 1
+fi
+sudo mkdir -p $TOOLCHAIN_DIR/gcc-linaro-arm-linux-gnueabihf-raspbian
+
+MACHINE_TYPE=`uname -m`
+if [ "${MACHINE_TYPE}" == "x86_64" ]; then
+  echo "Installing for x86_64 system"
+  sudo cp -r rpi_toolchain/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/* $TOOLCHAIN_DIR/gcc-linaro-arm-linux-gnueabihf-raspbian
+else
+  echo "Installing for x86_32 system"
+  sudo cp -r rpi_toolchain/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/* $TOOLCHAIN_DIR/gcc-linaro-arm-linux-gnueabihf-raspbian
+fi
+
+if [ "${TOOLCHAIN_DIR}" != "/opt/rpi_toolchain/" ]; then
+  exportline="export RPI_TOOLCHAIN_DIR=$TOOLCHAIN_DIR"
+  if grep -Fxq "$exportline" ~/.profile; then echo 'Already installed' ; else echo $exportline >> ~/.profile; fi
+fi
+
+echo -e "Cleaning up\n"
+rm -rf rpi_toolchain
+
+$TOOLCHAIN_DIR/gcc-linaro-arm-linux-gnueabihf-raspbian/bin/arm-linux-gnueabihf-gcc --version
+
+if [ $? -eq 0 ]; then
+    echo -e "Install complete"
+    exit 0
+else
+    echo "Install failed"
+    exit 1
+fi
+
---
+++ install_native.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+echo "Installing PX4 Native Toolchain for Raspberry Pi"
+echo "======================================================="
+sudo apt-get update && apt-get upgrade
+
+dpkg -i packages/*.deb
+sudo rm /usr/bin/cpp /usr/bin/gcc /usr/bin/g++
+sudo ln -s /usr/bin/cpp-4.8 /usr/bin/cpp
+sudo ln -s /usr/bin/gcc-4.8 /usr/bin/gcc
+sudo ln -s /usr/bin/g++-4.8 /usr/bin/g++
+
+echo 'OK'
+cmake --version
+echo ' '
+gcc -v
